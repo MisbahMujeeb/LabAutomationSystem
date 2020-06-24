a0277d285fdd02e16ad09857caa51712bf7ab6a4
MisbahMujeeb <misbah1810c@aptechgdn.net>
2020-06-24 23:55:37 +0500

    Template
    User Side Template

---
+++ userNavbar.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="it">
+  <meta name="keywords" content="Rapoo,creative, agency, startup,onepage, clean, modern,business, company,it">
+  
+  <meta name="author" content="themefisher.com">
+
+  <title>Rapoo- It solutions &amp; Corporate template</title>
+
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="userPlugins/bootstrap/css/bootstrap.min.css">
+  <!-- Animate Css -->
+  <link rel="stylesheet" href="userPlugins/animate-css/animate.css">
+  <!-- Icon Font css -->
+  <link rel="stylesheet" href="userPlugins/fontawesome/css/all.css">
+  <link rel="stylesheet" href="userPlugins/fonts/Pe-icon-7-stroke.css">
+  <!-- Themify icon Css -->
+  <link rel="stylesheet" href="userPlugins/themify/css/themify-icons.css">
+  <!-- Slick Carousel CSS -->
+  <link rel="stylesheet" href="userPlugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="userPlugins/slick-carousel/slick/slick-theme.css">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="usercss/style.css">
+
+</head>
+
+
+<body id="top-header">
+<div id="page-loader">
+    <div class="loader-icon fa fa-spin colored-border"></div>
+</div>
+<div class="logo-bar d-none d-md-block d-lg-block bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="logo d-none d-lg-block">
+                        <!-- Brand -->
+                        <a class="navbar-brand js-scroll-trigger" href="index.php" ><img src="images/log.png" alt="Logo" width="350px">
+                        </a>
+                    </div>
+                </div>
+
+                 <div class="col-lg-8 justify-content-end ml-lg-auto d-flex col-12">
+                    <div class="top-info-block d-inline-flex">
+                        <div class="icon-block">
+                            <i class="ti-mobile"></i>
+                        </div>
+                        <div class="info-block">
+                            <h5 class="font-weight-500">+7809346657</h5>
+                            <p>Call Free</p>
+                        </div>
+                    </div>
+
+                    <div class="top-info-block d-inline-flex">
+                        <div class="icon-block">
+                            <i class="ti-email"></i>
+                        </div>
+                        <div class="info-block">
+                            <h5 class="font-weight-500">abc@gmail.com</h5>
+                            <p>Email Us</p>
+                        </div>
+                    </div>
+                    <div class="top-info-block d-inline-flex">
+                        <div class="icon-block">
+                            <i class="ti-time"></i>
+                        </div>
+                        <div class="info-block">
+                            <h5 class="font-weight-500">Mon-Sat 9:00-12.00 </h5>
+                            <p>Sunday Closed</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- NAVBAR
+    ================================================= -->
+    <div class="main-navigation menu-2" id="mainmenu-area">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary main-nav navbar-togglable rounded-radius">
+
+                <a class="navbar-brand d-lg-none d-block" href="">
+                    <h4>SRS Electronic Appliances</h4>
+                </a>
+                <!-- Toggler -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="fa fa-bars"></span>
+                </button>
+
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <!-- Links -->
+                    <ul class="navbar-nav ">
+                        <li class="nav-item ">
+                            <a href="index.php" class="nav-link js-scroll-trigger">
+                               Home
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="about.php" class="nav-link js-scroll-trigger">
+                                About
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="contact.php" class="nav-link js-scroll-trigger">
+                               Contact
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="pricing.html" class="nav-link js-scroll-trigger">
+                                Pricing
+                            </a>
+                        </li>
+
+                        <li class="nav-item ">
+                            <a href="project.html" class="nav-link js-scroll-trigger">
+                                Projects
+                            </a>
+                        </li>
+
+                    </ul>
+<!-- 
+                    <ul class="ml-lg-auto list-unstyled m-0">
+                        <li><a href="#" class="btn btn-trans-white btn-circled">Get a quote</a></li>
+                    </ul> -->
+                </div> <!-- / .navbar-collapse -->
+            </nav>
+        </div> <!-- / .container -->
+    </div>
+
+
